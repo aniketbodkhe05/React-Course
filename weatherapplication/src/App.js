aee43ea6611c33e848157cce826c05202d5f87b6
Aniket Bodkhe <aniketbodkhe722@gmail.com>
2026-01-07 23:14:00 +0530

    Complete Weatherapplication

--- weatherapplication/src/App.js
+++ weatherapplication/src/App.js
@@ -1,14 +1,93 @@
-import logo from "./logo.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [city, setCity] = useState("Delhi");
+  const [weatherdata, setWeatherData] = useState(null);
+  const [error, setError] = useState("");
+
+  const currentDate = new Date();
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const month = months[currentDate.getMonth()];
+  const day = currentDate.getDate();
+  const year = currentDate.getFullYear();
+
+  const formattedDate = `${month} ${day}, ${year}`;
+
+  const API_KEY = "64341f4120e1dc087b390acc9bc46afc";
+
+  const fetchWeatherData = async () => {
+    try {
+      setError("");
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+      );
+
+      const data = await response.json();
+      if (data.cod === "404") {
+        setWeatherData(null);
+        setError("Location not found. Please enter a valid city.");
+        return;
+      }
+
+      setWeatherData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchWeatherData();
+  }, []);
+
+  const handleInputChange = (event) => {
+    console.log(event.target.value);
+    setCity(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    fetchWeatherData();
+  };
+
   return (
     <div className="App">
       <div className="container">
-        <h1 className="container_date">May 03,2025</h1>
-        <div className="weather_data">
-          <h2 className="container_city">Delhi</h2>
-        </div>
+        {error && <p className="error">{error}</p>}
+        {weatherdata && (
+          <>
+            <h1 className="container_date">{formattedDate}</h1>
+            <div className="weather_data">
+              <h2 className="container_city">{weatherdata.name}</h2>
+              <img className="container_img" src="/weather.jpg" alt="Weather" />
+              <h2 className="container_degree">{weatherdata.main.temp}°C</h2>
+              <h2 className="country_per">{weatherdata.weather[0].main}</h2>
+              <form className="form" onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  className="input"
+                  placeholder="Enter city name"
+                  onChange={handleInputChange}
+                />
+                <button type="submit">Get</button>
+              </form>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
